c5e7a9c0c9e8224ef095825cb5d131a8184b116c
Alex Adam <alex@eg312.com>
2018-04-20 22:06:23 +0300

    new menu style

--- client/components/menu.js
+++ client/components/menu.js
@@ -59,31 +59,35 @@ export default class Menu extends React.Component {
     }
 
     render = () => {
-        let menu = <button onClick={this.toggleExpand}>Options...</button>
+        let menu = <button className="menu-button" onClick={this.toggleExpand}>Show Options...</button>
 
         if (this.state.expanded) {
             menu = <div className="menu">
-                <button onClick={this.toggleExpand}>Options...</button>
+                <button className="menu-button" onClick={this.toggleExpand}>Hide Options</button>
                 <div className="menu-group">
-                    <label className="menu-label">Target Width:</label>
-                    <input type="number" className="menu-number-input" onChange={this.onWidthChange} value={this.state.targetWidth}/>
-                    <label className="menu-label">Target Height:</label>
-                    <input type="number" className="menu-number-input" onChange={this.onHeightChange} value={this.state.targetHeight}/>
+                    <div className="menu-sub-group">
+                        <label className="menu-label">Target Width:</label>
+                        <input type="number" className="menu-number-input" onChange={this.onWidthChange} value={this.state.targetWidth}/>
+                    </div>
+                    <div className="menu-sub-group">
+                        <label className="menu-label">Target Height:</label>
+                        <input type="number" className="menu-number-input" onChange={this.onHeightChange} value={this.state.targetHeight}/>
+                    </div>
                     <button className="menu-button" onClick={this.onDimenstionsChanged}>Apply</button>
                 </div>
                 <div className="menu-group">
                     <span>{this.props.inputImagesName[0] ? this.props.inputImagesName[0] : ''}</span>
-                    <label className="menu-button">Open file...<input type="file" className="menu-button menu-file-input" onChange={this.onFileUpload} /></label>
+                    <label className="menu-button">Open Image...<input type="file" className="menu-button menu-file-input" onChange={this.onFileUpload} /></label>
                 </div>
                 <div className="menu-group">
                     <label className="menu-label">Background Color:</label>
                     <input type="color" className="menu-button" value={this.state.backgroundColor} onChange={this.onBgColorChange} />
                 </div>
                 <div className="menu-group">
-                    <button className="menu-button" onClick={this.props.onFit}>Fit</button>
+                    <button className="menu-button" onClick={this.props.onFit}>Fit Image to Target</button>
                 </div>
                 <div className="menu-group">
-                    <button className="menu-button" onClick={this.props.onCenter}>Center</button>
+                    <button className="menu-button" onClick={this.props.onCenter}>Center Image</button>
                 </div>
                 <div className="menu-group">
                     <button className="menu-button" onClick={this.props.onSave}>Save...</button>
